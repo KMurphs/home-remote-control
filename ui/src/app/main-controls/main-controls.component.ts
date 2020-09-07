1f46794dd4acaf06103e2270bb7c17bd702761da
stephane kibonge <stephane.kibonge@etion.co.za>
2020-09-07 19:37:12 +0200

    Build for integration test run with backend

--- ui/src/app/main-controls/main-controls.component.ts
+++ ui/src/app/main-controls/main-controls.component.ts
@@ -13,16 +13,16 @@ export class MainControlsComponent implements OnInit {
 
   // https://material.io/resources/icons/?search=import&style=baseline
   buttons: TButtonControlData[] = [
-    { keyCode: "KEY_TOOLS", color: "", icon: "build", text: "Tool", ariaLabel: "Tools Key", ariaHidden: false, scaleFactor: 1.5 },
-    { keyCode: "KEY_UP", color: "accent", icon: "arrow_drop_up", text: "Up", ariaLabel: "Up Key", ariaHidden: false, scaleFactor: 3 },
+    { keyCode: "KEY_TOOLS", color: "", icon: "build", text: "Tool", ariaLabel: "Tools Key", ariaHidden: false, scaleFactor: 1 },
+    { keyCode: "KEY_VOLUP", color: "accent", icon: "arrow_drop_up", text: "Up", ariaLabel: "Up Key", ariaHidden: false, scaleFactor: 2.5 },
     { keyCode: "KEY_INFO", color: "", icon: "info", text: "Info", ariaLabel: "Info Key", ariaHidden: false, scaleFactor: 1 },
 
-    { keyCode: "KEY_LEFT", color: "accent", icon: "arrow_left", text: "Left", ariaLabel: "Left Key", ariaHidden: false, scaleFactor: 3 },
+    { keyCode: "KEY_LEFT", color: "accent", icon: "arrow_left", text: "Left", ariaLabel: "Left Key", ariaHidden: false, scaleFactor: 2.5 },
     { keyCode: "KEY_SOURCE", color: "primary", icon: "list", text: "Source", ariaLabel: "Source Key", ariaHidden: false, scaleFactor: 1 },
-    { keyCode: "KEY_RIGHT", color: "accent", icon: "arrow_right", text: "Right", ariaLabel: "Right Key", ariaHidden: false, scaleFactor: 3 },
+    { keyCode: "KEY_RIGHT", color: "accent", icon: "arrow_right", text: "Right", ariaLabel: "Right Key", ariaHidden: false, scaleFactor: 2.5 },
 
     { keyCode: "KEY_RETURN", color: "", icon: "backspace", text: "Return", ariaLabel: "Return Key", ariaHidden: false, scaleFactor: 1 },
-    { keyCode: "KEY_DOWN", color: "accent", icon: "arrow_drop_down", text: "Down", ariaLabel: "Down Key", ariaHidden: false, scaleFactor: 3 },
+    { keyCode: "KEY_VOLDOWN", color: "accent", icon: "arrow_drop_down", text: "Down", ariaLabel: "Down Key", ariaHidden: false, scaleFactor: 2.5 },
     { keyCode: "KEY_RETURN", color: "", icon: "close", text: "Exit", ariaLabel: "Exit Key", ariaHidden: false, scaleFactor: 1 }
   ]
 
