973d4a5c9d6e12c84fc18babb8916cfe9cb9cdc2
stephane kibonge <stephane.kibonge@etion.co.za>
2020-09-18 20:45:34 +0200

    Integrate mustache and ui. Successful Test against home tv

--- js/templates.js
+++ js/templates.js
@@ -1,22 +1,23 @@
-const templates = "\
+var template = "\
 \
 \
 {{#groups}}\
+  <!--<h1 class='uk-heading-divider uk-text-center'>{{name}}</h1>-->\
   <div class='remote-group uk-flex uk-flex-wrap'>\
-    <p>{{name}}</p>\
 \
     {{#entries}}\
       <div class='remote-group-item '>\
-        <button class='uk-button uk-button-secondary uk-flex uk-flex-column uk-flex-center' click='()=>{console.log('1223456')}'>\
-          <span uk-icon='icon: home; ratio: 1.5'></span>\
-          <span class='uk-text-small uk-margin-top'>Primary</span>\
+        <button class='uk-button uk-button-{{color}} uk-flex uk-flex-column uk-flex-center {{cssClass}}' onclick=\"fct('{{code}}')\">\
+          <span uk-icon='icon: {{icon}}; ratio: {{scaleFactor}}'></span>\
+          <span class='uk-text-small uk-margin-top'>{{text}}</span>\
         </button>\
       </div>\
     {{/entries}}\
 \
 \
   </div>\
-  <hr class='uk-divider-icon'></hr>\
+  <!--<hr class='uk-divider-icon'></hr>-->\
+  <h1 class='uk-heading-divider uk-text-center'></h1>\
 {{/groups}}\
 "
 
