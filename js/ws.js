35c80f6c55ca884fd87f01b5c18ba0fa7ec00908
stephane kibonge <stephane.kibonge@etion.co.za>
2020-09-18 20:56:11 +0200

    Add secure option for websocket

--- js/ws.js
+++ js/ws.js
@@ -1,18 +1,19 @@
 
 
-function DeviceWebsocket(deviceIP = "192.168.0.200", devicePort = 8001, clientName="samsung"){
+function DeviceWebsocket(deviceIP = "192.168.0.200", devicePort = 8001, clientName = "samsung", useWebsocketSecure = false){
 	if(!deviceIP) throw new Error("Device Object needs a valid IP Address")
 	if(!devicePort) throw new Error("Device Object needs a valid Port")
 	this.deviceIP = deviceIP;
 	this.devicePort = devicePort;
 	this.clientName = clientName;
+	this.useWebsocketSecure = useWebsocketSecure;
 	this.ws = null;
 	console.warn(deviceIP, devicePort, clientName)
 }
 DeviceWebsocket.prototype.version = "1.0"
 DeviceWebsocket.prototype.connect = function(onError = null, onMessage = null, onOpen = null, onClose = null){
 
-	this.ws = new WebSocket(`ws://${this.deviceIP}:${this.devicePort}/api/v2/channels/samsung.remote.control?name=${btoa(this.clientName)}`)
+	this.ws = new WebSocket(`${this.useWebsocketSecure ? 'wss' : 'ws'}://${this.deviceIP}:${this.devicePort}/api/v2/channels/samsung.remote.control?name=${btoa(this.clientName)}`)
 	
 	this.ws.onopen = function(evt) {
 		if(onOpen) onOpen(evt.data)
