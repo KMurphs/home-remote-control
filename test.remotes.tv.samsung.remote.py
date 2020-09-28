512ec9f2b8b81f2a6adc8d8d3f27e30fa714d2b0
stephane kibonge <stephane.kibonge@etion.co.za>
2020-09-28 10:16:17 +0200

    backup

--- test.remotes.tv.samsung.remote.py
+++ test.remotes.tv.samsung.remote.py
@@ -2,7 +2,7 @@ import sys
 from app.remotes.tv.samsung.remote import Remote
 
 
-remote = Remote({})
+remote = Remote({"ip":"192.168.0.200", "port":"8001"})
 remote.connect()
 remote.control(f"{sys.argv[1]}")
 remote.disconnect()
