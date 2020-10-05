e01e21a385b68ed5f97fa38f45d02d407e3f75aa
stephane kibonge <stephane.kibonge@etion.co.za>
2020-10-05 21:40:47 +0200

    Add text to loader. Add promise to DeviceWebsocket connect.

--- js/ws.js
+++ js/ws.js
@@ -16,33 +16,36 @@ function DeviceWebsocket(deviceIP, devicePort, clientName, useWebsocketSecure){
 DeviceWebsocket.prototype.version = "1.0";
 DeviceWebsocket.prototype.connect = function(){//(onError, onMessage, onOpen, onClose){
 
-	// if(!onError) onError = null;
-	// if(!onMessage) onMessage = null;
-	// if(!onOpen) onOpen = null;
-	// if(!onClose) onClose = null;
-	
-	this.ws = new WebSocket( (this.useWebsocketSecure ? 'wss' : 'ws') + "://" + this.deviceIP + ":" + this.devicePort + "/api/v2/channels/samsung.remote.control?name=" + btoa(this.clientName) );
+	return new Promise(function(resolve, reject){
+
+		this.ws = new WebSocket( (this.useWebsocketSecure ? 'wss' : 'ws') + "://" + this.deviceIP + ":" + this.devicePort + "/api/v2/channels/samsung.remote.control?name=" + btoa(this.clientName) );
 		
-	this.ws.onopen = function(evt) {
-		console.log('Socket is established with remote device');
-		// if(onOpen) onOpen(evt.data);
-	};
-	this.ws.onmessage = function(evt) {
-		console.log('Remote Device sent: ', evt);
-		// if(onMessage) onMessage(evt.data);
-	};  
-	this.ws.onclose = function(evt) {
-		// if(onClose) onClose(evt.data);
-    console.log('Socket is closed. Reconnect will be attempted in 1 second.', evt.reason);
-    setTimeout(function() {
-      this.ws.connect();
-    }, 1000);
-	}
-	this.ws.onerror = function(evt) {
-		// if(onError) onError(evt.data);
-		// else throw new Error(evt.data);
-		this.ws.close();
-	}
+		this.ws.onopen = function(evt) {
+			console.log('Socket is established with remote device');
+			resolve();
+			// if(onOpen) onOpen(evt.data);
+		};
+		this.ws.onmessage = function(evt) {
+			console.log('Remote Device sent: ', evt);
+			// if(onMessage) onMessage(evt.data);
+		};  
+		this.ws.onclose = function(evt) {
+			// if(onClose) onClose(evt.data);
+			console.log('Socket is closed. Reconnect will be attempted in 1 second.', evt.reason);
+			setTimeout(function() {
+				this.ws.connect();
+			}, 1000);
+		}
+		this.ws.onerror = function(evt) {
+			// if(onError) onError(evt.data);
+			// else throw new Error(evt.data);
+			this.ws.close();
+		}
+
+
+	})
+	
+
 }
 DeviceWebsocket.prototype.reconnect = function(){
 	this.ws = new WebSocket( (this.useWebsocketSecure ? 'wss' : 'ws') + "://" + this.deviceIP + ":" + this.devicePort + "/api/v2/channels/samsung.remote.control?name=" + btoa(this.clientName) );
