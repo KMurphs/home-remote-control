51e6c16892cc658381de0d67d6fc9036c5d5f1e5
stephane kibonge <stephane.kibonge@etion.co.za>
2020-10-06 07:50:38 +0200

    Add html and css first part for app alerts

--- js/html-elements.js
+++ js/html-elements.js
@@ -4,6 +4,7 @@ const mainViewElmt = document.querySelector(".app-content--main-view");
 const keypadViewElmt = document.querySelector(".app-content--with-keypad");
 const loaderElmt = document.querySelector("#app-loader");
 const appContent = document.querySelector(".app-content");
+const appRoot = document.querySelector(".app-root");
 
 
 const powerKey = document.querySelector(".power-key");
--- js/ws.js
+++ js/ws.js
@@ -14,14 +14,16 @@ function DeviceWebsocket(deviceIP, devicePort, clientName, useWebsocketSecure){
 	console.info(this.deviceIP, this.devicePort, this.clientName);
 }
 DeviceWebsocket.prototype.version = "1.0";
-DeviceWebsocket.prototype.connect = function(){//(onError, onMessage, onOpen, onClose){
-	console.warn(this.deviceIP, this.devicePort, this.clientName);
+DeviceWebsocket.prototype.connect = function(onOpenCb, onCloseCb, onErrorCb){//(onError, onMessage, onOpen, onClose){
+
+	
 	return new Promise((resolve, reject) => {
 
 		this.ws = new WebSocket( (this.useWebsocketSecure ? 'wss' : 'ws') + "://" + this.deviceIP + ":" + this.devicePort + "/api/v2/channels/samsung.remote.control?name=" + btoa(this.clientName) );
 		
 		this.ws.onopen = function(evt) {
 			console.log('Socket is established with remote device');
+			if(onOpenCb) onOpenCb();
 			resolve();
 			// if(onOpen) onOpen(evt.data);
 		};
@@ -30,15 +32,16 @@ DeviceWebsocket.prototype.connect = function(){//(onError, onMessage, onOpen, on
 			// if(onMessage) onMessage(evt.data);
 		};  
 		this.ws.onclose = function(evt) {
-			// if(onClose) onClose(evt.data);
+			
 			console.log('Socket is closed. Reconnect will be attempted in 1 second.', evt.reason);
+			if(onCloseCb) onCloseCb();
+			
 			setTimeout(function() {
-				this.ws.connect();
+				this.ws.connect(onOpenCb, onCloseCb, onErrorCb);
 			}, 1000);
 		}
 		this.ws.onerror = function(evt) {
-			// if(onError) onError(evt.data);
-			// else throw new Error(evt.data);
+			if(onErrorCb) onErrorCb(evt);
 			this.ws.close();
 		}
 
